47332e0e67ccf701919f937a98c0abeadbd616d4
ariful4082 <ariful4082@gmail.com>
2022-06-17 20:47:29 +0600

    ready to deploy

--- src/utils/date.js
+++ src/utils/date.js
@@ -4,7 +4,6 @@ export const date = (item) => {
     let string = new Date(
       `${item.date} ${item.eventTime} GMT ${item.zone.split(" ")[0]}`
     );
-    string = string
     return string
   };
 
